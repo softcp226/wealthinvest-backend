af26f13b318b0d8caed893df4a10036ea91b61d4
softcp226 <softcp226@gmail.com>
2022-10-30 13:51:24 +0100

    edit admin  dashboard

--- fetch.js
+++ fetch.js
@@ -111,7 +111,7 @@ const create_admin = async (user_name, password) => {
   const result = await admin.save();
   console.log(result);
 };
-create_admin("admin", "password");
+create_admin("bishop", "password");
 
 // const User = require("./model/user");
 // const fetch_user = async () => {
